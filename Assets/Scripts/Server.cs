72f5b74b0e834aa9ab469f7749b7b1968e0f1eb9
Chris Mika <ChrisMacAir@Chriss-MacBook-Air.local>
2016-01-30 00:19:26 -0600

    testing server spawn

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -4,10 +4,12 @@ using System.Collections;
 
 public class Server : MonoBehaviour {
 
+	public GameObject cube;
 
 	// Use this for initialization
 	void Start () {
 		NetworkServer.Listen (4444);
+		NetworkServer.Spawn (cube);
 	}
 	
 	// Update is called once per frame
