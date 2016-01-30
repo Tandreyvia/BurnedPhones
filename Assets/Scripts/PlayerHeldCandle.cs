275edf9144bde44a044e90bd50961035caa5fcfd
lucia <fairyprimula@gmail.com>
2016-01-30 17:01:22 -0600

    meow prefabs missing

---
+++ Assets/Scripts/PlayerHeldCandle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHeldCandle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(GetComponentInParent<PlayerUnit>().holdingCandle)
+        {
+            GetComponent<MeshRenderer>().enabled = true;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+        }
+	}
+}
