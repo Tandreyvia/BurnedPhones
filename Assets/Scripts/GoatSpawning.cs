153a4d7cd731be6f10fdc1accd4f94eea8b25ce8
Chris <chrismika93@gmail.com>
2016-01-31 13:38:43 -0600

    goat spawns

--- Assets/Scripts/GoatSpawning.cs
+++ Assets/Scripts/GoatSpawning.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class GoatSpawning : MonoBehaviour {
+public class GoatSpawning : NetworkBehaviour {
     public float timeBetweenSpawns = 8.0f;
     public float timeUntilNextSpawn;
 
@@ -9,20 +10,22 @@ public class GoatSpawning : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        timeUntilNextSpawn = 0.5f;
+		timeUntilNextSpawn = timeBetweenSpawns;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (timeUntilNextSpawn <= 0.0f)
-        {
-            Bounds targetBounds = GetComponent<BoxCollider2D>().bounds;
-            Vector3 goatTarget = new Vector3((targetBounds.center.x - targetBounds.extents.x) + (Random.value * targetBounds.extents.x * 2),
-                                               (targetBounds.center.y - targetBounds.extents.y) + (Random.value * targetBounds.extents.y * 2),
-                                                0);
-            GameObject.Instantiate(kawaii, goatTarget, transform.rotation);
+		if(isServer) {
+			timeUntilNextSpawn -= Time.smoothDeltaTime;
+			if (timeUntilNextSpawn <= 0.0f) {
+				Bounds targetBounds = GetComponent<BoxCollider2D> ().bounds;
+				Vector3 goatTarget = new Vector3 ((targetBounds.center.x - targetBounds.extents.x) + (Random.value * targetBounds.extents.x * 2),
+					                              (targetBounds.center.y - targetBounds.extents.y) + (Random.value * targetBounds.extents.y * 2),
+					                              0);
+				NetworkServer.Spawn((GameObject)GameObject.Instantiate (kawaii, goatTarget, transform.rotation));
 
-            timeBetweenSpawns += timeBetweenSpawns;
+				timeUntilNextSpawn += timeBetweenSpawns;
+			}
         }
     }
 }
